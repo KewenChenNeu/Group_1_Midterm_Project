ef2ae6f35da74c2d12dd1e0f7c38934ea09ec112
Fibi <zy94@cornell.edu>
2025-11-10 22:25:58 -0800

    fix the pre-record data

---  DigitalUniversitySystem/src/info5100/university/example/Persona/Transcript.java
+++  DigitalUniversitySystem/src/info5100/university/example/Persona/Transcript.java
@@ -108,7 +108,7 @@ public class Transcript {
     }
 
     public Iterable<CourseLoad> getCourseloadlist() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return courseloadlist.values();
     }
 
 }
